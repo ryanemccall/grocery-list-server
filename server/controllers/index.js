7821687831c3959a7c32ea904ed6e1046abbbdb2
Ryan Modrall <ryanemccall20@gmail.com>
2021-07-27 20:34:33 -0400

    Testing merge post folder move

--- server/controllers/index.js
+++
@@ -1,7 +0,0 @@
-//we create an object with key val pairs
-//basically we are importing the controller files through index.js so they can be used in app.js
-
-module.exports = {
-    groceryController: require("./groceryController"),
-    userController: require("./userController"),
-};
--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-const UserModel = require("./user");
-const GroceryModel = require("./grocery");
-
-module.exports = { UserModel, GroceryModel};
